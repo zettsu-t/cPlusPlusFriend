6f549ac26a292d550d235cd259f7e519a96a6920
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2017-05-17 20:52:32 +0900

    Add struct field testing

--- cppFriendsClang.cpp
+++ cppFriendsClang.cpp
@@ -27,7 +27,9 @@ namespace SwitchCase {
 
         return actual;
     }
+}
 
+namespace MemoryOperation {
     uint32_t g_shortBuffer[2];
     uint32_t g_largeBuffer[1024];
 
@@ -40,6 +42,61 @@ namespace SwitchCase {
         // rex64 jmp memset # TAILCALL
         ::memset(g_largeBuffer, 0, sizeof(g_largeBuffer));
     }
+
+    struct BitFields {
+        unsigned int member1 : 2;
+        unsigned int member2 : 4;
+        unsigned int member3 : 4;
+        unsigned int member4 : 5;
+        unsigned int : 0;
+    };
+
+    void SetBitFields(BitFields& fields) {
+        fields.member1 = 1;
+        fields.member2 = 3;
+        fields.member3 = 7;
+        fields.member4 = 15;
+        // 11' 1101'1100'1101b = 15,821
+        return;
+   }
+
+    void SetBitFieldsAtOnce(BitFields& fields) {
+        const BitFields origin = {1, 3, 7, 15};
+        fields = origin;
+        return;
+   }
+
+#if 0
+    // g++ -std=gnu++14 -Wall -IC:\MinGW\include
+    // -O2 -S -masm=intel -mavx2 -o cppFriendsClang.s cppFriendsClang.cpp
+    // 両関数ともこのコードになる
+    movzx   eax, BYTE PTR [rcx]
+    and eax, -64
+    or  eax, 13
+    mov BYTE PTR [rcx], al
+    movzx   eax, WORD PTR [rcx]
+    and ax, -961
+    or  ax, 448
+    mov WORD PTR [rcx], ax
+    shr ax, 8
+    and eax, -125
+    or  eax, 60
+    mov BYTE PTR 1[rcx], al
+    ret
+
+    // clang++ -std=gnu++14 -Wall --target=x86_64-pc-windows-gnu
+    // -isystem C:\MinGW\include -isystem C:\MinGW\lib\gcc\x86_64-w64-mingw32\6.3.0\include
+    // -O2 -S -masm=intel -mavx2 -o cppFriendsClang.s cppFriendsClang.cpp
+    // SetBitFields
+    movzx   eax, word ptr [rcx]
+    and eax, 32768
+    or  eax, 15821
+    mov word ptr [rcx], ax
+    ret
+    // SetBitFieldsAtOnce
+    mov dword ptr [rcx], 15821
+    ret
+#endif
 }
 
 /*
