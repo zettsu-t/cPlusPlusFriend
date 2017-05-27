5822b626ae90c7fe792784fa28c525de699f18d2
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2017-05-27 14:39:46 +0900

    Change -I to -isystem

--- cppFriendsSpace.bat
+++ cppFriendsSpace.bat
@@ -1,5 +1,5 @@
 set MINGWPATH=C:\MinGW\
 set PATH=%MINGWPATH%bin;%MINGWPATH%lib;%PATH%
-g++ -std=gnu++14 -Wall -I%MINGWPATH%include -DCPPFRIENDS_REGEX_BUILD_STAND_ALONE -o cppFriendsSpace cppFriendsSpace.cpp -lboost_regex
+g++ -std=gnu++14 -Wall -isystem%MINGWPATH%include -DCPPFRIENDS_REGEX_BUILD_STAND_ALONE -o cppFriendsSpace cppFriendsSpace.cpp -lboost_regex
 cppFriendsSpace
 set /P DUMMY="done"
