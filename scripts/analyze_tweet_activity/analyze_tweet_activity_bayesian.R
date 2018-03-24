3d695f1631a12f809dec2cd371288e23357c41f4
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2018-03-25 07:07:59 +0900

    Draw charts

--- scripts/analyze_tweet_activity/analyze_tweet_activity_bayesian.R
+++ scripts/analyze_tweet_activity/analyze_tweet_activity_bayesian.R
@@ -1,10 +1,12 @@
 ## Analyze tweet impressions by posted timestamps
 
+library(coda)
 library(data.table)
 library(rstan)
 library(ggplot2)
 
 infilename <- 'data/converted2.csv'
+pngfilename <- 'data/converted2.png'
 d <- data.table(read.csv(file=file.path(infilename),
                          header=TRUE, fileEncoding='utf-8',
                          stringsAsFactors=FALSE))
@@ -16,3 +18,8 @@ d_night <- d[time_index == 3]
 
 data <- list(N_morning=nrow(d_morning), N_night=nrow(d_night), Y_morning=log(d_morning$impressions), Y_night=log(d_night$impressions))
 fit <- stan(file='analyze_tweet_activity_bayesian.stan', data=data, seed=123)
+fit.coda<-mcmc.list(lapply(1:ncol(fit),function(x) mcmc(as.array(fit)[,x,])))
+
+png(filename=pngfilename, width=1200, height=800)
+plot(fit.coda)
+dev.off()
