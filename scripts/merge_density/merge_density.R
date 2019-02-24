525b78b8c8e78db372fba7b1ed41ad7f4e677bf7
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2019-02-24 12:01:38 +0900

    Merge distributions in R and C++

--- scripts/merge_density/merge_density.R
+++ scripts/merge_density/merge_density.R
@@ -9,7 +9,7 @@ merge_density <- function(density_left, density_right) {
     max_right <- length(density_right) - 1
     max_joint <- max_left + max_right
 
-    rowSums(sapply(0:max_right, function(index) {
+    vec <- rowSums(sapply(0:max_right, function(index) {
         ## 1-based index
         density <- density_right[index + 1]
         ## joint probability
@@ -17,17 +17,6 @@ merge_density <- function(density_left, density_right) {
         n_fill <- max_joint - max_left - index
         c(rep(0, index), likelihoods, rep(0, n_fill))
     }))
-}
-
-plot_density <- function(density_left, density_right, expected_var) {
-    vec <- merge_density(density_left, density_right)
-    xs <- 0:(NROW(vec)-1)
-    g <- ggplot()
-    g <- g + geom_line(aes(x=xs, y=vec),
-                       color="navy", alpha=0.8, linetype="solid")
-    g <- g + geom_line(aes(x=xs, y=dnorm(xs, max(xs)/2+0.1, sqrt(expected_var))),
-                       color="violet", alpha=0.8, linetype="dashed")
-    plot(g)
 
     print(sum(vec))
     df <- data.table(count=0:(NROW(vec)-1), freq=vec)
@@ -37,5 +26,46 @@ plot_density <- function(density_left, density_right, expected_var) {
     vec
 }
 
-vec <- plot_density(dnorm(0:40, 20, 3), dnorm(0:80, 40, 7), 58)
-vec <- plot_density(vec, dnorm(0:80, 40, 5), 83)
+plot_norm_density <- function(vec, expected_mean, expected_var) {
+    xs <- 0:(NROW(vec)-1)
+    g <- ggplot()
+    g <- g + geom_line(aes(x=xs, y=vec),
+                       color="navy", alpha=0.8, linetype="solid")
+    g <- g + geom_line(aes(x=xs, y=dnorm(xs, expected_mean, sqrt(expected_var))),
+                       color="violet", alpha=0.8, linetype="dashed")
+    plot(g)
+}
+
+plot_pois_density <- function(vec, expected_mean) {
+    xs <- 0:(NROW(vec)-1)
+    g <- ggplot()
+    g <- g + geom_line(aes(x=xs, y=vec),
+                       color="navy", alpha=0.8, linetype="solid")
+    g <- g + geom_line(aes(x=xs, y=dpois(xs, expected_mean)),
+                       color="violet", alpha=0.8, linetype="dashed")
+    plot(g)
+}
+
+plot_density <- function(vec, max_x) {
+    n <- min(max_x + 1, NROW(vec))
+    xs <- 0:(n-1)
+    ys <- vec[1:n]
+    g <- ggplot()
+    g <- g + geom_line(aes(x=xs, y=ys), color="navy", alpha=0.8, linetype="solid")
+    plot(g)
+}
+
+vec <- merge_density(dnorm(0:40, 20, 3), dnorm(0:80, 40, 7))
+plot_norm_density(vec, 60, 58)
+vec <- merge_density(vec, dnorm(0:80, 40, 5))
+plot_norm_density(vec, 100, 83)
+
+vec <- merge_density(dpois(0:30, 3), dpois(0:30, 4))
+plot_pois_density(vec, 7)
+vec <- merge_density(dpois(0:30, 3), dpois(0:30, 8))
+plot_pois_density(vec, 11)
+
+vec <- merge_density(dnbinom(0:100, size=10, prob=0.3), dnbinom(0:100, size=15, prob=0.8))
+vec <- merge_density(vec, dnbinom(0:100, size=30, prob=0.6))
+vec <- merge_density(vec, dnbinom(0:100, size=40, prob=0.55))
+plot_density(vec, 150)
--- scripts/merge_density/merge_density.cpp
+++ scripts/merge_density/merge_density.cpp
@@ -11,6 +11,39 @@ using Mean = double;
 using ProbabilityDensity = std::vector<Probability>;
 constexpr std::mt19937::result_type GlobalSeed = 123;
 
+template <typename T>
+ProbabilityDensity MakeDistribution(Value maxNumber, Value trialSize, T& dist) {
+    std::mt19937 gen{GlobalSeed};
+    ProbabilityDensity prob(maxNumber + 1);
+    const Probability unit = 1.0 / trialSize;
+    for(auto i = decltype(trialSize){0}; i < trialSize; ++i) {
+        Value num = std::round(dist(gen));
+        if ((num >= 0) && (num <= maxNumber)) {
+            prob.at(num) += unit;
+        }
+    }
+
+    return prob;
+}
+
+ProbabilityDensity MakeNormalDistribution(Mean mean, Mean variance, Value maxNumber, Value trialSize) {
+    std::mt19937 gen{GlobalSeed};
+    std::normal_distribution<Probability> dist(mean, std::sqrt(variance));
+    return MakeDistribution(maxNumber, trialSize, dist);
+}
+
+ProbabilityDensity MakePoissonDistribution(Mean mean, Value maxNumber, Value trialSize) {
+    std::mt19937 gen{GlobalSeed};
+    std::poisson_distribution<Value> dist(mean);
+    return MakeDistribution(maxNumber, trialSize, dist);
+}
+
+ProbabilityDensity MakeNegativeBinomialDistribution(Value size, Mean prob, Value maxNumber, Value trialSize) {
+    std::mt19937 gen{GlobalSeed};
+    std::negative_binomial_distribution<Value> dist(size, prob);
+    return MakeDistribution(maxNumber, trialSize, dist);
+}
+
 // Probability density from 0 to n (n+1 elements)
 ProbabilityDensity MergeProbabilityDensity(const ProbabilityDensity& lhs, const ProbabilityDensity& rhs) {
     if (lhs.empty()) {
@@ -34,38 +67,6 @@ ProbabilityDensity MergeProbabilityDensity(const ProbabilityDensity& lhs, const 
     }
 
     return jointProb;
-}
-
-ProbabilityDensity MakeNormalDistribution(Mean mean, Mean variance, Value maxNumber, Value trialSize) {
-    std::mt19937 gen{GlobalSeed};
-    std::normal_distribution<Probability> dist(mean, std::sqrt(variance));
-
-    ProbabilityDensity prob(maxNumber + 1);
-    const Probability unit = 1.0 / trialSize;
-    for(auto i = decltype(trialSize){0}; i < trialSize; ++i) {
-        Value num = std::round(dist(gen));
-        if ((num >= 0) && (num <= maxNumber)) {
-            prob.at(num) += unit;
-        }
-    }
-
-    return prob;
-}
-
-ProbabilityDensity MakePoissonDistribution(Mean mean, Value maxNumber, Value trialSize) {
-    std::mt19937 gen{GlobalSeed};
-    std::poisson_distribution<Value> dist(mean);
-
-    ProbabilityDensity prob(maxNumber + 1);
-    const Probability unit = 1.0 / trialSize;
-    for(auto i = decltype(trialSize){0}; i < trialSize; ++i) {
-        Value num = dist(gen);
-        if ((num >= 0) && (num <= maxNumber)) {
-            prob.at(num) += unit;
-        }
-    }
-
-    return prob;
 }
 
 struct Moments {
@@ -119,11 +120,25 @@ TEST_F(TestMergeProbabilityDensity, Poisson) {
     EXPECT_EQ(std::round(expectedVariance), std::round(actual.variance));
 }
 
+TEST_F(TestMergeProbabilityDensity, NegativeBinomial) {
+    constexpr Value size = 10;
+    constexpr Mean probToSuccess = 0.4;
+    constexpr Mean expectedMean = static_cast<Mean>(size) * (1.0 - probToSuccess) / probToSuccess;
+    constexpr Mean expectedVariance = expectedMean / probToSuccess;
+    constexpr Value maxNumber = 100;
+    constexpr Value trialSize = 1000000;
+
+    const auto dist = MakeNegativeBinomialDistribution(size, probToSuccess, maxNumber, trialSize);
+    const auto actual = GetMeanVariance(dist, 0.001);
+    EXPECT_EQ(expectedMean, std::round(actual.mean));
+    EXPECT_EQ(std::round(expectedVariance), std::round(actual.variance));
+}
+
 TEST_F(TestMergeProbabilityDensity, SumNormals) {
     const std::vector<Moments> testCases {{10, 16}, {50, 25}, {60, 36}, {70, 49}};
     const std::vector<Moments>::size_type testCaseSize = testCases.size();
     constexpr Value maxNumber = 150;
-    constexpr Value trialSize = 1000000;
+    constexpr Value trialSize = 100000;
 
     for(auto size = decltype(testCaseSize){1}; size <= testCaseSize; ++size) {
         ProbabilityDensity accumDist;
@@ -135,6 +150,59 @@ TEST_F(TestMergeProbabilityDensity, SumNormals) {
             expectedMean += mean;
             expectedVariance += variance;
             const auto dist = MakeNormalDistribution(mean, variance, maxNumber, trialSize);
+            accumDist = MergeProbabilityDensity(accumDist, dist);
+        }
+
+        const auto actual = GetMeanVariance(accumDist, 0.1);
+        EXPECT_TRUE(((expectedMean - 1) < actual.mean) && (actual.mean < (expectedMean + 1)));
+        EXPECT_TRUE(((expectedVariance - 1) < actual.variance) && (actual.variance < (expectedVariance + 1)));
+    }
+}
+
+TEST_F(TestMergeProbabilityDensity, SumPoisson) {
+    const std::vector<Mean> testCases {4, 8, 12, 16};
+    const std::vector<Moments>::size_type testCaseSize = testCases.size();
+    constexpr Value maxNumber = 255;
+    constexpr Value trialSize = 100000;
+
+    for(auto size = decltype(testCaseSize){1}; size <= testCaseSize; ++size) {
+        ProbabilityDensity accumDist;
+        Mean expected = 0.0;
+        for(auto i = decltype(size){0}; i < size; ++i) {
+            auto mean = testCases.at(i);
+            expected += mean;
+            const auto dist = MakePoissonDistribution(mean, maxNumber, trialSize);
+            accumDist = MergeProbabilityDensity(accumDist, dist);
+        }
+
+        const auto actual = GetMeanVariance(accumDist, 0.1);
+        EXPECT_TRUE(((expected - 1) < actual.mean) && (actual.mean < (expected + 1)));
+        EXPECT_TRUE(((expected - 1) < actual.variance) && (actual.variance < (expected + 1)));
+    }
+}
+
+TEST_F(TestMergeProbabilityDensity, SumNegativeBinomial) {
+    struct Params {
+        Value size {0};
+        Mean probToSuccess {0.0};
+    };
+
+    const std::vector<Params> testCases {{3, 0.8}, {8, 0.6}, {10, 0.45}, {20, 0.7}};
+    const std::vector<Params>::size_type testCaseSize = testCases.size();
+    constexpr Value maxNumber = 150;
+    constexpr Value trialSize = 100000;
+
+    for(auto size = decltype(testCaseSize){1}; size <= testCaseSize; ++size) {
+        ProbabilityDensity accumDist;
+        Mean expectedMean = 0.0;
+        Mean expectedVariance = 0.0;
+        for(auto i = decltype(size){0}; i < size; ++i) {
+            const auto& testCase = testCases[i];
+            auto mean = static_cast<Mean>(testCase.size) * (1.0 - testCase.probToSuccess) / testCase.probToSuccess;
+            auto variance = mean / testCase.probToSuccess;
+            expectedMean += mean;
+            expectedVariance += variance;
+            const auto dist = MakeNegativeBinomialDistribution(testCase.size, testCase.probToSuccess, maxNumber, trialSize);
             accumDist = MergeProbabilityDensity(accumDist, dist);
         }
 
