0f4c6c4648d47da3f4c4ff383e68469159b3a72a
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2020-05-10 00:12:46 +0100

    Add support vector regressions

--- scripts/stock_price/density.R
+++ scripts/stock_price/density.R
@@ -1,5 +1,6 @@
 library(assertthat)
 library(dplyr)
+library(e1071)
 library(exactRankTests)
 library(ggplot2)
 library(purrr)
@@ -21,6 +22,10 @@ df_pref <- df_pref %>%
     dplyr::filter(population > 0) %>%
     dplyr::mutate(population=log10(population), density=log10(density))
 
+df_pref_sub <- df_pref %>%
+    dplyr::arrange(population) %>%
+    dplyr::top_n(-38, population)
+
 ## 市区町村だけ取り出す
 df_city <- df_base %>%
     dplyr::filter(地域識別コード %in% c('1', '2', '3')) %>%
@@ -28,19 +33,31 @@ df_city <- df_base %>%
     dplyr::select(c(5,6,11))
 
 names(df_city) <-c('city', 'population', 'density')
+
+## 青ヶ島村より人口が少ない場合は除外する
 df_city <- df_city %>%
-    dplyr::filter(population > 0) %>%
-    dplyr::mutate(population=log10(population), density=log10(density))
+    dplyr::filter(population > 100) %>%
+    dplyr::mutate(population=log10(population), density=log10(density)) %>%
+    dplyr::arrange(density)
+
+svm_model_pref <- e1071::svm(density ~ population , df_pref)
+df_pref$predicted <- as.vector(predict(svm_model_pref, df_pref))
+
+svm_model_pref_sub <- e1071::svm(density ~ population , df_pref_sub)
+df_pref_sub$predicted <- as.vector(predict(svm_model_pref, df_pref_sub))
+
+svm_model_city <- e1071::svm(density ~ population , df_city)
+df_city$predicted <- as.vector(predict(svm_model_city, df_city))
 
 draw_chart <- function(input_df, label_font_size, png_filename) {
-    x_name <- setdiff(names(input_df), c('population', 'density'))
+    x_name <- setdiff(names(input_df), c('population', 'density'))[1]
     xs <- na.omit(input_df$population)
     ys <- na.omit(input_df$density)
     cor_value <- cor(xs, ys)
     cor_x <- median(xs)
     cor_y <- max(ys) - 1
     x_range <- ceiling(range(xs) * 4) / 4.0
-    x_breaks <- seq(from=x_range[1] - 0.25, to=x_range[2] + 0.5, by=0.25)
+    x_breaks <- seq(from=x_range[1] - 0.5, to=x_range[2] + 0.5, by=0.5)
 
     font_name <- 'Migu 1M'
     font_size <- 24
@@ -49,11 +66,17 @@ draw_chart <- function(input_df, label_font_size, png_filename) {
         png(png_filename, width = 1600, height = 900)
     }
     g <- ggplot(input_df, aes(x=population, y=density, label=!!(rlang::sym(x_name))))
-    g <- g + geom_point()
     g <- g + geom_label(alpha=0.6, family=font_name, size=label_font_size)
     g <- g + scale_x_continuous(breaks=x_breaks)
-    g <- g + geom_smooth(method='lm', formula=y~x)
-    g <- g + labs(title=paste(x_name, sprintf(" : cor=%.3f", cor_value)),
+    g <- g + geom_smooth(method='lm', formula=y~x, size=2)
+
+    suffix <- 'blue=LM'
+    if ('predicted' %in% names(input_df)) {
+        g <- g + geom_line(aes(x=population, y=predicted), color='orchid', size=2)
+        suffix <- 'blue=LM, orchid=SVR'
+    }
+
+    g <- g + labs(title=paste(x_name, sprintf(": cor=%.3f", cor_value), suffix),
                   x='log10 (population)', y='log10 (population/area)')
     g <- g + theme(text=element_text(family=font_name),
                    legend.position="top",
@@ -96,8 +119,4 @@ draw_random_chart <- function(png_filename) {
 draw_random_chart('random.png')
 draw_chart(input_df=df_pref, label_font_size=16, png_filename='pref_all.png')
 draw_chart(input_df=df_city, label_font_size=10, png_filename='city.png')
-
-df_pref_sub <- df_pref %>%
-    dplyr::arrange(population) %>%
-    dplyr::top_n(-38, population)
 draw_chart(input_df=df_pref_sub, label_font_size=16, png_filename='pref_sub.png')
