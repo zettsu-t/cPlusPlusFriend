66356e616346fe9a8fa7e050ad5ca78777f09bef
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2021-05-29 10:46:39 +0100

    Use updateSelectInput

--- living_time_survey/server.R
+++ living_time_survey/server.R
@@ -1,8 +1,11 @@
 library(shiny)
 library(plotly)
 
-render_local <- function(input) {
-  df <- filter_by_action(df = g_df_survey, action = input$action, days_in_week = input$days_in_week)
+reactive_list <- reactiveValues()
+reactive_list$df_survey <- read_data_all(in_dirname = "incoming")
+
+render_local <- function(input, use_plotly) {
+  df <- filter_by_action(df = reactive_list$df_survey, action = input$action, days_in_week = input$days_in_week)
 
   method <- "group"
   color_func <- pals::tol
@@ -10,7 +13,6 @@ render_local <- function(input) {
     method <- "cohort"
     color_func <- pals::stepped
   }
-  use_plotly <- (input$render == "plotly")
 
   draw_by_survey_years_for_webapp(
     df = df, action = input$action, days_in_week = input$days_in_week,
@@ -19,10 +21,16 @@ render_local <- function(input) {
 }
 
 shinyServer(function(input, output, session) {
+  observe({
+    reactive_list$action_set <- unique(reactive_list$df_survey$`行動`)
+    reactive_list$days_in_week_set <- unique(reactive_list$df_survey$`曜日`)
+    updateSelectInput(session, "action", label = "行動", choices = reactive_list$action_set)
+    updateSelectInput(session, "days_in_week", label = "曜日", choices = reactive_list$days_in_week_set)
+  })
   output$main_plot <- renderPlot({
-    render_local(input)
+    render_local(input, use_plotly = FALSE)
   })
   output$main_plotly <- renderPlotly({
-    render_local(input)
+    render_local(input, use_plotly = TRUE)
   })
 })
--- living_time_survey/ui.R
+++ living_time_survey/ui.R
@@ -1,26 +1,30 @@
 library(shiny)
+library(shinycssloaders)
+library(shinythemes)
 library(plotly)
 
 shinyUI(fluidPage(
   titlePanel("NHK放送文化研究所「国民生活時間調査」 全員平均時間量、男女年層別"),
   a(href = "https://www.nhk.or.jp/bunken/yoron-jikan/", "出典へのリンク"),
   fluidRow(
-    column(
+      theme = shinythemes::shinytheme("cerulean"),
+      column(
       2,
-      selectInput("action", label = "行動", choices = g_action_set),
-      selectInput("days_in_week", label = "曜日", choices = g_days_in_week_set),
+      selectInput("action", label = "行動", choices = c("睡眠")),
+      selectInput("days_in_week", label = "曜日", choices = c("平日")),
       selectInput("method", label = "年齢集団", choices = c("生年", "調査年")),
-      selectInput("render", label = "描画方法", choices = c("plot", "plotly"))
     ),
     column(
       10,
-      conditionalPanel(
-        condition = "input.render == 'plotly'",
-        plotlyOutput("main_plotly", height = "125%"), style = "height: 60vh"
-      ),
-      conditionalPanel(
-        condition = "input.render != 'plotly'",
-        plotOutput("main_plot", height = "125%"), style = "height: 50vh"
+      tabsetPanel(type = "tabs",
+        tabPanel("Plot",
+          plotOutput("main_plot", height = "125%"),
+          style = "height: 50vh"
+        ),
+        tabPanel("Plotly",
+          withSpinner(plotlyOutput("main_plotly", height = "125%")),
+          style = "height: 100vh"
+        )
       )
     )
   )
