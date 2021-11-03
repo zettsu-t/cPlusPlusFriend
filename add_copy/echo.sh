82a7f01db4f06c1e38d061341f3284fcd373b53c
zettsu-t <zettsu.tatsuya@gmail.com>
2021-11-03 05:57:43 +0100

    Copy deep and flatten

---
+++ add_copy/echo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find /root -type f
+cat /root/first1/1st.txt
+cat /root/first1/second1/2nd.txt
+cat /root/first2/1st.txt
+cat /root/first2/second2/2nd.txt
+cat /root/sub3/first3/second1/2nd.txt
+cat /root/sub4/first4/second2/2nd.txt
+wc /root/flat1/*.txt
+wc /root/flat2/*.txt
