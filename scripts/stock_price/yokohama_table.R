d4b82c1b8095e992577cc39d00b11e732eab3517
zettsu-t <zettsu.tatsuya@gmail.com>
2021-11-20 05:34:54 +0000

    Use a placeholder

--- scripts/stock_price/yokohama_table.R
+++ scripts/stock_price/yokohama_table.R
@@ -16,7 +16,7 @@ get_color_index <- function(wards, indexes) {
 }
 
 to_color_table <- function(df) {
-  df_gt <- df %>%
+  df %>%
     dplyr::mutate(index = row_number()) %>%
     dplyr::mutate(color = get_color_index(市区名, index)) %>%
     gt::gt() %>%
@@ -28,25 +28,25 @@ to_color_table <- function(df) {
     gt::tab_style(
       style = cell_fill(color = "azure"),
       locations = cells_body(rows = (color == 3))
-    )
+    ) %>%
+    purrr::reduce(.x=tail(seq_len(NCOL(df)), -3), .init=., function(acc, col) {
+      offset <- 1
+      values <- as.vector(unlist(df_population_data[-offset, col]))
+      ward_max <- offset + which.max(values)
+      ward_min <- offset + which.min(values)
 
-  purrr::reduce(.x=4:12, .init=df_gt, function(acc, col) {
-    offset <- 1
-    ward_max <- offset + which.max(as.vector(unlist(df_population_data[-offset, col])))
-    ward_min <- offset + which.min(as.vector(unlist(df_population_data[-offset, col])))
-
-    acc %>% gt::tab_style(
-      style = cell_fill(color = 'khaki1'),
-      locations = cells_body(
-        columns = c(col),
-        rows = c(ward_max)
-      )) %>% gt::tab_style(
-      style = cell_fill(color = 'khaki3'),
-      locations = cells_body(
-        columns = c(col),
-        rows = c(ward_min)
-      ))
-  }) %>%
+      acc %>% gt::tab_style(
+        style = cell_fill(color = 'khaki1'),
+        locations = cells_body(
+          columns = c(col),
+          rows = c(ward_max)
+        )) %>% gt::tab_style(
+          style = cell_fill(color = 'khaki3'),
+          locations = cells_body(
+            columns = c(col),
+            rows = c(ward_min)
+          ))
+    }) %>%
     gt::opt_table_font(font = "Migu 1M")
 }
 
