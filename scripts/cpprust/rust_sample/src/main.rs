e3ea9594d45e57cc744f3bda2b36ff5411090d01
zettsu-t <zettsu.tatsuya@gmail.com>
2022-08-24 21:23:29 +0100

    Use arbitrary precision integers

--- scripts/cpprust/rust_sample/src/main.rs
+++ scripts/cpprust/rust_sample/src/main.rs
@@ -1,13 +1,21 @@
+use num_bigint::BigInt;
+
 fn succ_i32(x: i32) -> i32 {
     x + 1
 }
 
 #[test]
-fn test_succ() {
+fn test_succ_i32() {
     let arg: i32 = 1;
     let expected: i32 = 2;
     assert_eq!(succ_i32(arg), expected);
 }
+
+// fn test_succ_i32_error() {
+//    let arg: i16 = 1;
+//    let expected: i16 = 2;
+//    assert_eq!(succ_i32(arg), expected);
+// }
 
 fn succ<T>(x: T) -> T
 where
@@ -17,7 +25,7 @@ where
 }
 
 #[test]
-fn test_succ_num() {
+fn test_succ() {
     let arg_i8: i8 = 1;
     let expected_i8: i8 = 2;
     assert_eq!(succ(arg_i8), expected_i8);
@@ -25,6 +33,10 @@ fn test_succ_num() {
     let arg_u8: u8 = 254;
     let expected_u8: u8 = 255;
     assert_eq!(succ(arg_u8), expected_u8);
+
+    let arg: i32 = 1;
+    let expected: i32 = 2;
+    assert_eq!(succ(arg), expected);
 
     let arg_i64: i64 = 0x700180029003a00f;
     let expected_i64: i64 = 0x700180029003a010;
@@ -34,5 +46,35 @@ fn test_succ_num() {
     let expected_i64: u128 = 0xff00ee00dd00cc00bb00aa0099010000;
     assert_eq!(succ(arg_i64), expected_i64);
 }
+
+// fn test_succ_error() {
+//    let arg: f32 = 1.0;
+//    let expected: f32 = 2.0;
+//    assert_eq!(succ(arg), expected);
+// }
+
+fn succ_big<T>(x: T) -> T
+where
+    T: std::ops::Add + std::ops::Add<Output = T> + std::convert::From<i8> + std::ops::BitAnd,
+{
+    x + 1_i8.into()
+}
+
+#[test]
+fn test_succ_big() {
+    let n = BigInt::parse_bytes(b"999999999999999999999999999999999", 10).unwrap();
+    let n_plus = BigInt::parse_bytes(b"1000000000000000000000000000000000", 10).unwrap();
+    assert_eq!(succ_big(n), n_plus);
+
+    let arg: i64 = 0x2000ffff;
+    let expected = 0x20010000;
+    assert_eq!(succ_big(arg), expected);
+}
+
+// fn test_succ_big_error() {
+//    let arg: f32 = 1.0;
+//    let expected: f32 = 2.0;
+//    assert_eq!(succ_big(arg), expected);
+// }
 
 fn main() {}
