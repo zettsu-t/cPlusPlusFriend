2873b05059f003107432a0daaf603faf524c9496
zettsu-t <zettsu.tatsuya@gmail.com>
2022-10-10 00:49:08 +0100

    Add app tests

--- scripts/juliaset/rust/juliaset/tests/cli_test.rs
+++ scripts/juliaset/rust/juliaset/tests/cli_test.rs
@@ -56,7 +56,66 @@ fn count_temp_dir(dir: &Path, file_pattern: &str) -> usize {
 }
 
 #[test]
-fn test_correct_cli_arguments() -> Result<(), Box<dyn std::error::Error>> {
+fn test_correct_csv_arguments() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_dir = Builder::new().prefix("test-dir").rand_bytes(10).tempdir()?;
+    let temp_csv_filename = temp_dir.path().join("_test_.csv");
+    let csv_filename = temp_csv_filename.to_str().unwrap();
+
+    let mut cmd_success = Command::cargo_bin("appjuliaset")?;
+    let pixel_size: u32 = 8;
+    let pixel_size_str = format!("{}", pixel_size);
+    cmd_success
+        .arg("-x")
+        .arg("-0.875")
+        .arg("-y")
+        .arg("0.25")
+        .arg("-m")
+        .arg("20")
+        .arg("-s")
+        .arg(pixel_size_str)
+        .arg("-c")
+        .arg(csv_filename)
+        .assert()
+        .success();
+
+    let n_csvfiles = count_temp_dir(temp_dir.path(), "*.csv");
+    assert_eq!(n_csvfiles, 1 as usize);
+    let n_pngfiles = count_temp_dir(temp_dir.path(), "*.png");
+    assert_eq!(n_pngfiles, 0 as usize);
+
+    let mut reader = ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(csv_filename)?;
+    let mut n_columns: usize = 0;
+
+    type Count = i32;
+    let expected_max: Count = 20;
+    let mut actual_max: Count = 0;
+
+    for (i, row) in reader.records().enumerate() {
+        let cells: Vec<Count> = row?.iter().map(|s| s.parse().unwrap()).collect();
+        assert_eq!(cells.len(), pixel_size as usize);
+        let cell_sum = cells.iter().sum::<Count>();
+        let cell_max = *(cells.iter().max().unwrap_or(&0));
+        actual_max = std::cmp::max(actual_max, cell_max);
+
+        if (i == 0) | ((i + 1) == pixel_size.try_into().unwrap()) {
+            assert_eq!(0, cell_sum);
+        } else {
+            assert!(cell_sum > 0);
+        }
+
+        // 1-based indexing
+        n_columns = i + 1;
+    }
+
+    assert_eq!(actual_max, expected_max);
+    assert_eq!(n_columns, pixel_size as usize);
+    Ok(())
+}
+
+#[test]
+fn test_correct_image_arguments() -> Result<(), Box<dyn std::error::Error>> {
     let temp_dir = Builder::new().prefix("test-dir").rand_bytes(10).tempdir()?;
     let temp_png_filename = temp_dir.path().join("_test_.png");
     let png_filename = temp_png_filename.to_str().unwrap();
@@ -80,49 +139,7 @@ fn test_correct_cli_arguments() -> Result<(), Box<dyn std::error::Error>> {
     let actual_image = ImageReader::open(png_filename)?.decode()?;
     assert_eq!(actual_image.width(), pixel_size);
     assert_eq!(actual_image.height(), pixel_size);
-
-    Ok(())
-}
-
-#[test]
-fn test_correct_xy_arguments() -> Result<(), Box<dyn std::error::Error>> {
-    let temp_dir = Builder::new().prefix("test-dir").rand_bytes(10).tempdir()?;
-    let temp_csv_filename = temp_dir.path().join("_test_.csv");
-    let csv_filename = temp_csv_filename.to_str().unwrap();
-
-    let mut cmd_success = Command::cargo_bin("appjuliaset")?;
-    let pixel_size: u32 = 8;
-    let pixel_size_str = format!("{}", pixel_size);
-    cmd_success
-        .arg("-x")
-        .arg("0.5")
-        .arg("-y")
-        .arg("0.125")
-        .arg("-m")
-        .arg("20")
-        .arg("-s")
-        .arg(pixel_size_str)
-        .arg("-c")
-        .arg(csv_filename)
-        .assert()
-        .success();
-
-    let n_csvfiles = count_temp_dir(temp_dir.path(), "*.csv");
-    assert_eq!(n_csvfiles, 1 as usize);
-    let n_pngfiles = count_temp_dir(temp_dir.path(), "*.png");
-    assert_eq!(n_pngfiles, 0 as usize);
-
-    let mut reader = ReaderBuilder::new()
-        .has_headers(false)
-        .from_path(csv_filename)?;
-    let mut n_columns: usize = 0;
-
-    for (i, row) in reader.records().enumerate() {
-        assert_eq!(row.unwrap().len(), pixel_size as usize);
-        // 1-based indexing
-        n_columns = i + 1;
-    }
-    assert_eq!(n_columns, pixel_size as usize);
+    assert!(!actual_image.to_rgb8().iter().find(|&x| x > &0).is_none());
 
     Ok(())
 }
